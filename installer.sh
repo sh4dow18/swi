d2d0c2b2b9479c83629b84b427dfa8097abb7a19
sh4dow18 <ramses.solano.arias@gmail.com>
2021-11-24 13:05:46 -0600

    Fixing Installations Problems

--- installer.sh
+++ installer.sh
@@ -38,17 +38,17 @@ echo -e "\e[1;31m\e[1;1m\nLoading..."
 sleep 7
 clear
 
-mkdir libraries
-mkdir config
-mkdir programs
+mkdir libraries 2>/dev/null
+mkdir config 2>/dev/null
+mkdir programs 2>/dev/null
 
-echo -e "\e[1;31   ___                  __             ___             ___           __"
-echo -e "\e[1;31  / _ \\___ _    _____  / /__  ___ ____/ (_)__  ___ _  / _ \___ _____/ /_____ ____ ____ ___"
-echo -e "\e[1;31 / // / _ \ |/|/ / _ \\/ / _ \\/ _ \`/ _  / / _ \/ _ \`/ / ___/ _ \`/ __/  '_/ _ \`/ _ \`/ -_|_-<"
-echo -e "\e[1;31/____/\\___/__,__/_//_/_/\\___/\\_,_/\\_,_/_/_//_/\\_, / /_/   \\_,_/\\__/_/\\_\\\\_,_/\\_, /\\__/___/"
-echo -e "\e[1;31                                             /___/                          /___/"
+echo -e "\e[1;31m   ___                  __             ___             ___           __"
+echo -e "\e[1;31m  / _ \\___ _    _____  / /__  ___ ____/ (_)__  ___ _  / _ \\___ _____/ /_____ ____ ____ ___"
+echo -e "\e[1;31m / // / _ \\ |/|/ / _ \\/ / _ \\/ _ \`/ _  / / _ \\/ _ \`/ / ___/ _ \`/ __/  '_/ _ \`/ _ \`/ -_|_-<"
+echo -e "\e[1;31m/____/\\___/__,__/_//_/_/\\___/\\_,_/\\_,_/_/_//_/\\_, / /_/   \\_,_/\\__/_/\\_\\\\_,_/\\_, /\\__/___/"
+echo -e "\e[1;31m                                             /___/                          /___/"
 
-echo -e "\e[1;32\nSearching programs to download..."
+echo -e "\e[1;32m\nSearching programs to download...\n"
 sleep 3
 
 cd config/
@@ -64,25 +64,30 @@ wget http://security.ubuntu.com/ubuntu/pool/main/p/python3.8/libpython3.8_3.8.10
 cd ../programs/
 wget https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community_8.0.27-1ubuntu20.04_amd64.deb
 
-echo -e "\e[1;32\nLoading..."
+echo -e "\e[1;33m\nLoading..."
 sleep 3
 clear
 
-echo -e "\e[1;33   ____         __       _____             ___"
-echo -e "\e[1;33  /  _/__  ___ / /____ _/ / (_)__  ___ _  / _ \\_______  ___ ________ ___ _  ___"
-echo -e "\e[1;33 _/ // _ \\(_-</ __/ _ \`/ / / / _ \\/ _ \`/ / ___/ __/ _ \\/ _ \`/ __/ _ \`/  ' \(_-<"
-echo -e "\e[1;33/___/_//_/___/\\__/\_,_/_/_/_/_//_/\\_, / /_/  /_/  \\___/\\_, /_/  \\_,_/_/_/_/___/"
-echo -e "\e[1;33                                 /___/                /___/"
+echo -e "\e[1;34m   ____         __       _____             ___"
+echo -e "\e[1;34m  /  _/__  ___ / /____ _/ / (_)__  ___ _  / _ \\_______  ___ ________ ___ _  ___"
+echo -e "\e[1;34m _/ // _ \\(_-</ __/ _ \`/ / / / _ \\/ _ \`/ / ___/ __/ _ \\/ _ \`/ __/ _ \`/  ' \(_-<"
+echo -e "\e[1;34m/___/_//_/___/\\__/\_,_/_/_/_/_//_/\\_, / /_/  /_/  \\___/\\_, /_/  \\_,_/_/_/_/___/"
+echo -e "\e[1;34m                                 /___/                /___/"
 
-echo -e "\e[1;34\n\t- Installing MySql"
-# sudo apt install ./mysql-apt-config_0.8.20-1_all.deb
-# sudo apt update
-# sudo apt install mysql-server
-echo -e "\e[1;35\n\t- Installing Required Libraries"
-# sudo apt install ./libproj15_6.3.1-1_amd64.deb ./libzip5_1.5.1-0ubuntu1_amd64.deb ./libc6_2.31-0ubuntu9_amd64.deb ./libmpdec2_2.4.2-3_amd64.deb ./libpython3.8-minimal_3.8.10-0ubuntu1~20.04.1_amd64.deb ./libpython3.8-stdlib_3.8.10-0ubuntu1~20.04.1_amd64.deb ./libpython3.8_3.8.10-0ubuntu1~20.04.1_amd64.deb
-echo -e "\e[1;36\n\t- Installing MySql Workbench"
-# sudo apt install ./mysql-workbench-community_8.0.27-1ubuntu20.04_amd64.deb
-echo -e "\e[1;31\n\t- Installing Keyrings"
-# sudo apt install gnome-keyring
-echo -e "\e[1;32\nThe installation has been completed successfully :D"
+cd ../config/
+echo -e "\e[1;35m\n- Installing MySql Preconfiguration"
+sudo apt install -y ./mysql-apt-config_0.8.20-1_all.deb
+echo -e "\e[1;36m\n- Updating Packages"
+sudo apt update
+echo -e "\e[1;31m\n- Installing MySql-Server"
+sudo apt install -y mysql-server
+cd ../libraries/
+echo -e "\e[1;32m\n- Installing Required Libraries"
+sudo apt install -y ./libproj15_6.3.1-1_amd64.deb ./libzip5_1.5.1-0ubuntu1_amd64.deb ./libc6_2.31-0ubuntu9_amd64.deb ./libmpdec2_2.4.2-3_amd64.deb ./libpython3.8-minimal_3.8.10-0ubuntu1~20.04.1_amd64.deb ./libpython3.8-stdlib_3.8.10-0ubuntu1~20.04.1_amd64.deb ./libpython3.8_3.8.10-0ubuntu1~20.04.1_amd64.deb
+echo -e "\e[1;33m\n- Installing MySql Workbench"
+cd ../programs/
+sudo apt install -y ./mysql-workbench-community_8.0.27-1ubuntu20.04_amd64.deb
+echo -e "\e[1;34m\n- Installing Keyrings"
+sudo apt install -y gnome-keyring
+echo -e "\e[1;35m\nThe installation has been completed successfully :D"
 sleep 3
